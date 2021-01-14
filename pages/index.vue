14562d6a11a585e451f12302134911c2fdecec66
Marcel Pfeifer <marcel@wolves.ink>
2021-01-14 10:52:10 +0100

    feat: :sparkles: Add light and dark mode

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,37 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">twitchgames</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div>
+			<h1>Twitch Games</h1>
+			<p>Wilkommen bei Twitch Games. Hier hast du die Möglichkeit mit deinem Chat Spiele zu spielen.</p>
+
+			<div class="channelInputWrapper">
+				<input
+					id="channelName"
+					v-model="channelName"
+					class="input"
+					placeholder="0to100ink"
+					@keydown.enter="saveChannel"
+				/>
+				<label for="channelName">Twitch Channel Name</label>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+	margin: 0 auto;
+	min-height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.channelInputWrapper {
+	margin: 2em 0;
 }
 </style>
